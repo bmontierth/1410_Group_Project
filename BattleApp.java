625367b9816964343ccd54b7f230265034ed4667
taylorbasso <taylor008@gmail.com>
2016-04-27 16:20:37 -0600

    Delete BattleApp.java

--- BattleApp.java
+++
@@ -1,9 +0,0 @@
-/*****************************************
- * Assignment: 1410 Group Project
- * Class : ${CLASS_NAME}
- * Author : Brian Montierth
- * Created : 4/12/2016
- ******************************************/
-public class BattleApp {
-
-}
