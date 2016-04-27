8c7ef9db917c0192a0afb2f4667fe2a61200408c
taylorbasso <taylor008@gmail.com>
2016-04-27 16:45:53 -0600

    No Code Change - Just JavaDoc

--- Hero.java
+++ Hero.java
@@ -2,13 +2,22 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+/**
+ * Class Hero extends Character. Hero is going to be who the user controls, the user also chooses the hero class
+ * and name. It has getters and setters for all stats and the name. It also contains the hero level, which is
+ * used to scale enemies difficulty.
+ * @author Taylor
+ *
+ */
 public class Hero extends Character {
+	
 	private int level;
 	private final String heroClass;
 	private final Icon fullImage;
 	private final Icon faceImage;
+	
 	/**
-	 * Constructor to create a Hero.
+	 * Constructor to create a Hero. 
 	 * @param CLASS One of the classes from Enum InitHeroClass.
 	 * @param name The name of the Hero.
 	 */
@@ -21,45 +30,62 @@ public class Hero extends Character {
 	}
 
 	/**
-	 * @return the level
+	 * get the level of the hero
+	 * @return the level of the hero Object.
 	*/
 	public int getLevel() {
 		return level;
 	}
 
 	/**
-	 * @param level the level to set
+	 * setter for the level of the hero
+	 * @param level the level to set for the Hero object.
 	 */
 	public void setLevel(int level) {
 		this.level = level;
 	}
 
 	/**
-	 * @return the hero's class
+	 * getter for the class of the hero. There is no setter for this value, it is a FINAL field
+	 * @return the hero's class 
 	 */
 	public String getHeroClass() {
 		return heroClass;
 	}
 
+	/**
+	 * toString gets the hero's name that was set by the user and then concatenates on the Class of that hero.
+	 * @return a string of "(HeroName) the (HeroClass)" to use in the GUI
+	 */
 	@Override
 	public String toString() {
 		return super.getName() + " the " + getHeroClass();
 	}
 
 	/**
-	 * @return the fullImage
+	 * fullImage is an Icon of the entire Hero.
+	 * @return the fullImage of the hero
 	 */
 	public Icon getFullImage() {
 		return fullImage;
 	}
 
 	/**
-	 * @return the faceImage
+	 * faceImage is the cropped image of the hero's face.
+	 *
+	 * @return the faceImage of the Hero
 	 */
 	public Icon getFaceImage() {
 		return faceImage;
 	}
 
+	/**
+	 * levelUp is called when the hero levels up after defeating an enemy. It is a noarg, no return method, and calls 
+	 * the method setLevel.
+	 * levelUp has a JOptionPane which contains four values that the user can choose to levelUp. The user gets base
+	 * stat increases on all fronts, but the user is able to choose in the JOptionPane one stat to levelUp again. Based on
+	 * which stat is chosen by the user, that choice is increased again.
+	 */
 	public void levelUp()
 	{
 		this.setLevel(this.getLevel() + 1);
@@ -91,9 +117,6 @@ public class Hero extends Character {
 					this.setCrit(this.getCrit() + 1);
 					break;
 			}
-		
 	}
 	
 }
-
-
--- HeroTest.java
+++ HeroTest.java
@@ -2,9 +2,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Class HeroTest tests the Hero.java class. Hero.java extends Character and has getters and
+ * setters for all the hero stats (like attack, defense, crit, health, etc.
+ * 
+ * HeroTest initializes a new Hero to run tests on. We check that the hero is an instanceof Character
+ * as well as Hero. We then make sure the toString method works correctly.
+ * @author Taylor
+ */
 public class HeroTest {
 	Hero hero = new Hero(InitHeroClass.WARRIOR,"Bardok");
 	
+	/**
+	 * testHero() tests to make sure that our instantiated hero is an instanceof Character and Hero using the assertTrue Method.
+	 * no param, no return method.
+	 */
 	@Test
 	public void testHero() {
 		assertTrue(hero instanceof Character);
@@ -12,6 +24,11 @@ public class HeroTest {
 		
 	}
 
+	/**
+	 * testToString() makes sure that when we pass the hero object to its toString method, it returns the hero
+	 * name concatenated with it's class.
+	 * no param, no return.
+	 */
 	@Test
 	public void testToString() {
 		String testName = "Bardok the Warrior";
