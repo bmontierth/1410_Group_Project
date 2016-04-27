63c37425aefd747adc8f3b1a32c55dd313a4d196
bmontierth <bmontier@bruinmail.slcc.edu>
2016-04-26 18:20:23 -0600

    Added files via upload

--- InitHeroClass.java
+++ InitHeroClass.java
@@ -1,3 +1,6 @@
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 /*****************************************
  * Assignment: 1410 Group Project
  * Class : ${CLASS_NAME}
@@ -5,19 +8,27 @@
  * Created : 4/7/2016
  ******************************************/
 public enum InitHeroClass {
-    WARRIOR(12,11,11,6),ROGUE(10,8,9,13),KNIGHT(8,13,14,5),BERSERKER(14,5,8,13);
+    ARCHER(12,11,11,6,new ImageIcon(InitHeroClass.class.getResource("/imgss/cutearcher.png")),new ImageIcon(InitHeroClass.class.getResource("/imgss/cutearcherhead.png")))
+    ,MAGE(10,8,9,13,new ImageIcon(InitHeroClass.class.getResource("/imgss/cutemage.png")), new ImageIcon(InitHeroClass.class.getResource("/imgss/cutemadehead.png")))
+    ,ROGUE(8,13,14,5,new ImageIcon(InitHeroClass.class.getResource("/imgss/cuterogue.png")),new ImageIcon(InitHeroClass.class.getResource("/imgss/cuteroguehead.png")))
+    ,WARRIOR(14,5,8,13,new ImageIcon(InitHeroClass.class.getResource("/imgss/cuteswordguy.png")),new ImageIcon(InitHeroClass.class.getResource("/imgss/cuteswordhead.png")));
 
     public final int ATTACK;
     public final int DEFENSE;
     public final int HEALTH;
     public final int CRIT;
+    public final Icon fullImage;
+    public final Icon faceImage;
 
 
-    InitHeroClass(int attack, int defense, int health, int crit) {
+    InitHeroClass(int attack, int defense, int health, int crit, Icon fullImage, Icon faceImage) {
         this.ATTACK = attack;
         this.DEFENSE = defense;
         this.HEALTH = health;
         this.CRIT = crit;
+        this.fullImage = fullImage;
+        this.faceImage = faceImage;
+        
     }
 
     @Override
