10a9d84babaec66cfc62dd7736b2172031da4a1f
taylorbasso <taylor008@gmail.com>
2016-04-26 18:18:54 -0600

    Enemy and Hero from 4/26 class

--- Enemy.java
+++ Enemy.java
@@ -2,14 +2,13 @@ package groupProject;
 
 import java.util.Random;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 /*****************************************
  * Assignment: 1410 Group Project Class : ${CLASS_NAME} Author : Brian Montierth
  * Created : 4/7/2016
  ******************************************/
-// -Create multiple enemies
-// Different stats
-// -Stats scale off of hero level
-//
 
 public class Enemy extends Character
 	{
@@ -21,7 +20,7 @@ public class Enemy extends Character
 		 * @param crit
 		 * @param name
 		 */
-		private Enemy(int attack, int defense, int health, int crit, String name)
+		private Enemy(int attack, int defense, int health, int crit, String name, Icon fullImage, Icon faceImage)
 			{
 				super(attack, defense, health, crit, name);
 			}
@@ -29,7 +28,7 @@ public class Enemy extends Character
 		public static Character randomEnemy(int heroLevel)
 			{
 				String[] enemyArray =
-					{ "Golem", "Zombie", "Demon", "Flying Demon", "Skeleton", "Dragon", "Vampire" };
+					{ "Golem", "Zombie", "Demon", "Skeleton", "Dragon", "Vampire" };
 
 				int randAttack = 1;
 				int randDefense = 1;
@@ -44,12 +43,34 @@ public class Enemy extends Character
 				for (int stat = 0; stat < stats.length; stat++)
 					{
 						stats[stat] = (int) (rand.nextInt(enemyArray.length) + 8 + heroScale);
-						System.out.println("the stat in the loop: " + stats[stat]);
 					}
+				
+				int random = rand.nextInt(enemyArray.length);
+				Icon body = null;
+				Icon face = null;
+				
+				switch (random) {
+					case 0: body = new ImageIcon(Enemy.class.getResource("/imgss/golemboss.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/golembosshead.png"));
+					break;
+					case 1: body = new ImageIcon(Enemy.class.getResource("/imgss/meeseeksZ.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/meeseeksZhead.png"));
+					break;
+					case 2: body = new ImageIcon(Enemy.class.getResource("/imgss/demon.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/demonhead.png"));
+					break;
+					case 3: body = new ImageIcon(Enemy.class.getResource("/imgss/skelly.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/skellyhead.png"));
+					break;
+					case 4: body = new ImageIcon(Enemy.class.getResource("/imgss/dragon.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/dragonhead.png"));
+					break;
+					case 5: body = new ImageIcon(Enemy.class.getResource("/imgss/vampire.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/vampirehead.png"));
+					break;
+				}
 
-				System.out.println("stat at zero outside the loop: " + stats[0]);
-
-				return new Enemy(stats[0], stats[1], stats[2], stats[3], enemyArray[rand.nextInt(enemyArray.length)]);
+				return new Enemy(stats[0], stats[1], stats[2], stats[3], enemyArray[random], body, face);
 			}
 
 		/*
@@ -62,31 +83,53 @@ public class Enemy extends Character
 			{
 				return super.getName();
 			}
-		
+
 		public static Character finalBoss(int heroLevel)
-		{
-			String[] enemyArray =
-						{ "Golem", "Zombie", "Demon", "Flying Demon", "Skeleton", "Dragon", "Vampire" };
+			{
+				String[] enemyArray =
+					{ "Golem", "Zombie", "Demon", "Skeleton", "Dragon", "Vampire" };
 
-					int randAttack = 1;
-					int randDefense = 1;
-					int randHealth = 1;
-					int randCrit = 1;
+				int randAttack = 1;
+				int randDefense = 1;
+				int randHealth = 1;
+				int randCrit = 1;
 
-					int[] stats =
-						{ randAttack, randDefense, randHealth, randCrit };
-					Random rand = new Random();
-					double heroScale = (heroLevel * .9);
+				int[] stats =
+					{ randAttack, randDefense, randHealth, randCrit };
+				Random rand = new Random();
+				double heroScale = (heroLevel * .9);
 
-					for (int stat = 0; stat < stats.length; stat++)
-						{
-							stats[stat] = (int) (rand.nextInt(enemyArray.length) + 8 + heroScale);
-							System.out.println("the stat in the loop: " + stats[stat]);
-						}
+				for (int stat = 0; stat < stats.length; stat++)
+					{
+						stats[stat] = (int) (rand.nextInt(enemyArray.length) + 8 + heroScale);
+					}
 
-					System.out.println("stat at zero outside the loop: " + stats[0]);
+				int random = rand.nextInt(enemyArray.length);
+				Icon body = null;
+				Icon face = null;
+				
+				switch (random) {
+					case 0: body = new ImageIcon(Enemy.class.getResource("/imgss/golemboss.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/golembosshead.png"));
+					break;
+					case 1: body = new ImageIcon(Enemy.class.getResource("/imgss/meeseeksZ.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/meeseeksZhead.png"));
+					break;
+					case 2: body = new ImageIcon(Enemy.class.getResource("/imgss/demon.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/demonhead.png"));
+					break;
+					case 3: body = new ImageIcon(Enemy.class.getResource("/imgss/skelly.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/skellyhead.png"));
+					break;
+					case 4: body = new ImageIcon(Enemy.class.getResource("/imgss/dragon.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/dragonhead.png"));
+					break;
+					case 5: body = new ImageIcon(Enemy.class.getResource("/imgss/vampire.png"));
+					face = new ImageIcon(Enemy.class.getResource("/imgss/vampirehead.png"));
+					break;
+				}
 
-					return new Enemy(stats[0], stats[1], stats[2], stats[3], enemyArray[rand.nextInt(enemyArray.length)] + " Boss");
-		}
+				return new Enemy(stats[0], stats[1], stats[2], stats[3], enemyArray[random] + " Boss", body, face);
+			}
 
 	}
--- Hero.java
+++ Hero.java
@@ -1,7 +1,14 @@
+package groupProject;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 
 public class Hero extends Character {
 	private int level;
 	private final String heroClass;
+	private final Icon fullImage;
+	private final Icon faceImage;
 	/**
 	 * Constructor to create a Hero.
 	 * @param CLASS One of the classes from Enum InitHeroClass.
@@ -11,6 +18,8 @@ public class Hero extends Character {
 		super(CLASS.ATTACK, CLASS.DEFENSE, CLASS.HEALTH, CLASS.CRIT, name);
 		this.level = 1;
 		this.heroClass = CLASS.toString();
+		this.fullImage = CLASS.fullImage;
+		this.faceImage = CLASS.faceImage;
 	}
 
 	/**
@@ -39,9 +48,53 @@ public class Hero extends Character {
 		return super.getName() + " the " + getHeroClass();
 	}
 
-	public void levelUp(){
-		level++;
+	/**
+	 * @return the fullImage
+	 */
+	public Icon getFullImage() {
+		return fullImage;
 	}
-}
 
+	/**
+	 * @return the faceImage
+	 */
+	public Icon getFaceImage() {
+		return faceImage;
+	}
 
+	
+
+		public void levelUp()
+			{
+				this.setLevel(this.getLevel() + 1);
+				String[] buttons =
+					{ "Attack", "Defense", "Health", "Crit" };
+
+				int returnValue = JOptionPane.showOptionDialog(null, "Select a stat to level up", "Level Up!",
+						JOptionPane.PLAIN_MESSAGE, 0,
+						new ImageIcon(Battle.class.getResource("/groupProject.imgss/ArcherLady.png")), buttons,
+						buttons[0]);
+
+				this.setAttack(this.getAttack() + 3);
+				this.setDefense(this.getDefense() + 2);
+				this.setHealth(this.getHealth() + 10);
+				this.setCrit(this.getCrit() + 1);
+
+				switch (returnValue)
+					{
+						case 0:
+							this.setAttack(this.getAttack() + 3);
+							break;
+						case 1:
+							this.setDefense(this.getDefense() + 2);
+							break;
+						case 2:
+							this.setHealth(this.getHealth() + 10);
+							break;
+						case 3:
+							this.setCrit(this.getCrit() + 1);
+							break;
+					}
+				
+			}
+	}
